658bd794e1cad29bb080c4162a69c7f605f72e34
Riyaz <shaikhriyaz434@gmail.com>
2023-12-25 21:21:10 +0800

    day 2

--- learn-die/main.go
+++ learn-die/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Go, Gophers ${fuckers}")
+
+	fmt.Println("What you think we are doing here")
+	inReader := bufio.NewReader(os.Stdin)
+	userInput, err := inReader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error while reading:%v", err)
+	}
+	result := "Yes, we are motherfucket! " + strings.ToUpper(userInput)
+	fmt.Println(result)
 }
---
+++ learn-die/sample.js
@@ -0,0 +1,11 @@
+
+const fs=require('fs');
+
+console.log("Life of pi");
+
+const one="why this is not working";
+
+const template=`life of pi returns with : ${one}`;
+
+
+
